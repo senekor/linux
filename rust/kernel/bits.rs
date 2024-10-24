dce550305d434140b2aea66c9d382fed7c9f2196
Daniel Almeida <daniel.almeida@collabora.com>
2024-10-24 11:17:47 -0300

    rust: kernel: add support for bits/genmask macros
    These macros are converted from their kernel equivalent.
    
    Note by Remo: During a rebase, this file seems to have been added to the
    mainline kernel. The macros it contains all had different names though.
    So I just kept the content from both sides. Subsequent patches that rely
    on the "outdated" version of this file should thus still work. But, this
    is not clean at all and should be looked at in detail before anything
    that depends on it is considered for upstreaming.

--- rust/kernel/bits.rs
+++ rust/kernel/bits.rs
@@ -201,3 +201,34 @@ impl_genmask_fn!(
     /// assert_eq!(genmask_u8(0..=7), u8::MAX);
     /// ```
 );
+
+/// Produces a literal where bit `n` is set.
+///
+/// Equivalent to the kernel's `BIT` macro.
+///
+#[macro_export]
+macro_rules! bit {
+    ($n:expr) => {
+        (1 << $n)
+    };
+}
+
+/// Create a contiguous bitmask starting at bit position `l` and ending at
+/// position `h`, where `h >= l`.
+///
+/// # Examples
+/// ```
+///     use kernel::genmask;
+///     let mask = genmask!(39, 21);
+///     assert_eq!(mask, 0x000000ffffe00000);
+/// ```
+///
+#[macro_export]
+macro_rules! genmask {
+    ($h:expr, $l:expr) => {{
+        const _: () = {
+            assert!($h >= $l);
+        };
+        ((!0u64 - (1u64 << $l) + 1) & (!0u64 >> (64 - 1 - $h)))
+    }};
+}

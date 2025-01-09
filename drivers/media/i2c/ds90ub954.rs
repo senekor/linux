230d1baf80ae5f351c8d6d6a3a05f8d3d48da583
Remo Senekowitsch <remo@buenzli.dev>
2025-01-09 15:25:04 +0100

    parse devicetree (ds90ub954)

--- drivers/media/i2c/ds90ub954.rs
+++ drivers/media/i2c/ds90ub954.rs
@@ -5,7 +5,7 @@
 //!
 //! Datasheet: https://www.ti.com/lit/ds/symlink/ds90ub954-q1.pdf
 
-use kernel::{c_str, i2c, of, prelude::*};
+use kernel::{c_str, gpio::consumer as gpio, i2c, of, prelude::*, regmap};
 
 kernel::module_i2c_driver! {
     type: Ds90ub954,
@@ -38,18 +38,18 @@ kernel::of_device_table!(
 
 struct Ds90ub954 {
     i2c_client: i2c::Client,
+    pass_gpio: Option<gpio::Desc>,
+    lock_gpio: Option<gpio::Desc>,
+    pdb_gpio: Option<gpio::Desc>,
     // struct regmap *regmap;
     // struct ds90ub953_priv *ser[NUM_SERIALIZER]; //serializers
-    // int pass_gpio;
-    // int lock_gpio;
-    // int pdb_gpio;
     // int sel_rx_port; // selected rx port
     // int sel_ia_config; // selected ia configuration
-    // int csi_lane_count;
-    // int csi_lane_speed;
-    // int test_pattern;
+    csi_lane_count: u32,
+    csi_lane_speed: u32,
+    test_pattern: bool,
     // int num_ser; // number of serializers connected
-    // int conts_clk; // continuous clock (0: discontinuous, 1: continuous)
+    continuous_clock: bool, // continuous clock (0: discontinuous, 1: continuous)
 }
 
 impl i2c::Driver for Ds90ub954 {
@@ -61,16 +61,128 @@ impl i2c::Driver for Ds90ub954 {
     fn probe(client: &mut i2c::Client, id_info: Option<&Self::IdInfo>) -> Result<Pin<KBox<Self>>> {
         pr_info!("Hello from DS90UB954 driver\n");
 
+        let dev = client.as_ref();
+
+        let Ds90ub954ParseDtReturn {
+            pass_gpio,
+            lock_gpio,
+            pdb_gpio,
+            csi_lane_count,
+            csi_lane_speed,
+            test_pattern,
+            continuous_clock,
+        } = ds90ub954_parse_dt(dev, id_info).map_err(|e| {
+            dev_err!(dev, "error parsing device tree\n");
+            e
+        })?;
+
         let driver_data = Self {
             i2c_client: client.clone(),
+            pass_gpio,
+            lock_gpio,
+            pdb_gpio,
+            csi_lane_count,
+            csi_lane_speed,
+            test_pattern,
+            continuous_clock,
         };
         let driver_data = KBox::new(driver_data, GFP_KERNEL)?;
         Ok(driver_data.into())
     }
 }
 
+fn ds90ub954_parse_dt(
+    dev: &kernel::device::Device,
+    id_info: Option<&()>,
+) -> Result<Ds90ub954ParseDtReturn> {
+    if id_info.is_none() {
+        dev_err!(dev, "Failed to find matching dt id\n");
+        return Err(ENODEV);
+    }
+
+    let try_get_gpio = |con_id: &'static CStr, flags: gpio::Flags| -> Result<Option<gpio::Desc>> {
+        match gpio::Desc::get(dev, con_id, flags) {
+            Ok(gpio) => Ok(Some(gpio)),
+            Err(e) if e == EPROBE_DEFER => {
+                dev_err!(dev, "{con_id}-gpio read failed: (EPROBE_DEFER)\n");
+                Err(e)
+            }
+            Err(_) => {
+                dev_info!(dev, "{con_id}-gpio not found, ignoring\n");
+                Ok(None)
+            }
+        }
+    };
+    let pass_gpio = try_get_gpio(c_str!("pass"), gpio::Flags::In)?;
+    let lock_gpio = try_get_gpio(c_str!("lock"), gpio::Flags::In)?;
+    let pdb_gpio = try_get_gpio(c_str!("pdb"), gpio::Flags::OutLow)?;
+
+    let csi_lane_count = dev
+        .property_read::<u32>(c_str!("csi-lane-count"), None)
+        .unwrap_or_else(|_| {
+            dev_info!(
+                dev,
+                "csi-lane-count property not found, set to default value\n"
+            );
+            4
+        });
+    dev_info!(dev, "csi-lane-count: {csi_lane_count}\n");
+
+    let csi_lane_speed = dev
+        .property_read::<u32>(c_str!("csi-lane-speed"), None)
+        .unwrap_or_else(|_| {
+            dev_info!(
+                dev,
+                "csi-lane-speed property not found, set to default value\n"
+            );
+            1600
+        });
+    dev_info!(dev, "csi-lane-speed: {csi_lane_speed}\n");
+
+    let test_pattern = dev.property_read_bool(c_str!("test-pattern"));
+    if test_pattern {
+        dev_info!(dev, "test-pattern enabled\n");
+    } else {
+        dev_info!(dev, "test-pattern disabled\n");
+    }
+
+    let continuous_clock = dev.property_read_bool(c_str!("continuous-clock"));
+    if continuous_clock {
+        dev_info!(dev, "continuous clock enabled\n");
+    } else {
+        dev_info!(dev, "discontinuous clock used\n");
+    }
+
+    Ok(Ds90ub954ParseDtReturn {
+        pass_gpio,
+        lock_gpio,
+        pdb_gpio,
+        csi_lane_count,
+        csi_lane_speed,
+        test_pattern,
+        continuous_clock,
+    })
+}
+struct Ds90ub954ParseDtReturn {
+    pass_gpio: Option<gpio::Desc>,
+    lock_gpio: Option<gpio::Desc>,
+    pdb_gpio: Option<gpio::Desc>,
+    csi_lane_count: u32,
+    csi_lane_speed: u32,
+    test_pattern: bool,
+    continuous_clock: bool,
+}
+
 impl Drop for Ds90ub954 {
     fn drop(&mut self) {
+        // TODO
+        //
+        // ds90ub953_free(priv);
+        // ds90ub954_pwr_disable(priv);
+        if let Some(mut pdb_gpio) = self.pdb_gpio.as_mut() {
+            pdb_gpio.set_value_cansleep(0);
+        }
+
         pr_info!("Goodbye from DS90UB954 driver\n");
     }
 }

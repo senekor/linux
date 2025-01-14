8ab31da664aadf64fb3e924c9b8eda88686d8dd8
Remo Senekowitsch <remo@buenzli.dev>
2025-01-14 18:06:04 +0100

    rust: delay: add msleep function
    Signed-off-by: Remo Senekowitsch <remo@buenzli.dev>

---
+++ rust/kernel/delay.rs
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0
+
+//! Delay routines, using a pre-computed "loops_per_jiffy" value.
+//! Sleep routines using timer list timers or hrtimers.
+//!
+//! C header: [`include/linux/delay.h`](srctree/include/linux/delay.h).
+
+use crate::bindings;
+
+/// Sleeps for the specified number of milliseconds.
+pub fn msleep(msecs: u32) {
+    // SAFETY: The behavior of msleep it defined for the full range of `u32`.
+    unsafe { bindings::msleep(msecs) }
+}
